c26bf0f02406bfd68045d69fba8120beea97d2f0
kokanevina <kokane.vina@gmail.com>
2022-05-26 15:05:15 +0530

    Ways of Javascript
    inline, internal, external

---
+++ JAVASCRIPT/Scripts/MyScript.js
@@ -0,0 +1,7 @@
+
+let b1node=document.getElementById('b1');
+let b2node=document.getElementById('b2');
+if(b1node!=null)
+    b1node.style.backgroundColor='yellow';
+if(b2node!=null)
+b2node.style.backgroundColor='yellow';
